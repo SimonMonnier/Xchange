696a67987670cd7dfc311a21c837976b1cdecb39
Simon Monnier <s.monnier44440@gmail.com>
2025-06-07 10:25:52 +0200

    Enable core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,15 @@ plugins {
 android {
     namespace = "com.example.xchange"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Override the Flutter NDK version to satisfy plugin requirements.
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // Enable desugaring to support Java 8+ language features required by
+        // certain plugins such as flutter_local_notifications.
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +45,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Required for core library desugaring.
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
