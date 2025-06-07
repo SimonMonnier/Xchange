86b47a2800b774c170fb90c3d511d6b3506a03c0
Simon Monnier <s.monnier44440@gmail.com>
2025-06-07 22:14:45 +0200

    Add HTTP-based ad retrieval

--- lib/models/announcement.dart
+++ lib/models/announcement.dart
@@ -5,6 +5,7 @@ class Announcement {
   final double price;
   final String? imageUrl;
   final String? imageBase64;
+  final String? serverUrl;
   Announcement({
     required this.id,
     required this.title,
@@ -12,6 +13,7 @@ class Announcement {
     required this.price,
     this.imageUrl,
     this.imageBase64,
+    this.serverUrl,
   });
 
   factory Announcement.fromJson(Map<String, dynamic> json) {
@@ -22,6 +24,7 @@ class Announcement {
       price: (json['price'] as num).toDouble(),
       imageUrl: json['imageUrl'] as String?,
       imageBase64: json['imageBase64'] as String?,
+      serverUrl: json['serverUrl'] as String?,
     );
   }
 
@@ -32,5 +35,35 @@ class Announcement {
         'price': price,
         'imageUrl': imageUrl,
         'imageBase64': imageBase64,
+        'serverUrl': serverUrl,
       };
+
+  Map<String, dynamic> toAdvertiseJson() => {
+        'id': id,
+        'title': title,
+        'description': description,
+        'price': price,
+        'imageUrl': imageUrl,
+        'serverUrl': serverUrl,
+      };
+
+  Announcement copyWith({
+    String? id,
+    String? title,
+    String? description,
+    double? price,
+    String? imageUrl,
+    String? imageBase64,
+    String? serverUrl,
+  }) {
+    return Announcement(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      imageUrl: imageUrl ?? this.imageUrl,
+      imageBase64: imageBase64 ?? this.imageBase64,
+      serverUrl: serverUrl ?? this.serverUrl,
+    );
+  }
 }
