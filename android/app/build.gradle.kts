2de25806c8e951dca51fca8fd7171425dd220655
Simon Monnier <s.monnier44440@gmail.com>
2025-06-07 10:21:39 +0200

    fix build issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.xchange"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Override the Flutter NDK version to satisfy plugin requirements.
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
